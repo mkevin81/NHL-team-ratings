fadadb04448b2e6f736aff6af867ee593cd5c0b0
emkay81 <lesentreprisesmkquintin@hotmail.com>
2019-01-16 23:14:17 -0500

    Update team_ratings.py
    added ovr_rankings()
    fixed teams names

--- ratings/team_ratings.py
+++ ratings/team_ratings.py
@@ -10,10 +10,6 @@ from collections import Counter
 from sklearn.preprocessing import StandardScaler
 
 def fw_ratings(team):
-    
-    #Takes in 'team initials' or league' and returns expected goal differential per game vs. 
-    #average opponent dataframe. This rating doesnt take into account special teams, goalies or injuries.
-    
     url = 'http://www.puckon.net/?_ga=2.210512981.1165101973.1547008033-2109006389.1546797137'
     raw = pd.read_html(url,header=1)[0]
     raw_cut = raw[['Team','GP','SA.1','ESVA.1']]
@@ -26,35 +22,35 @@ def fw_ratings(team):
     sa = pd.pivot_table(new,values = 'SA.1',columns = 'Team')
     esva = pd.pivot_table(new,values = 'ESVA.1',columns = 'Team')
     rating_lst = []
-    
+
     if team in sa.columns:
         sa_1 = sa.loc['SA.1',f'{team}']
         esva_1 = esva.loc['ESVA.1',f'{team}']
         rating = -1.7051343491288098e-15 -0.19222278*sa_1 + 0.28562659*esva_1
         return rating
     elif team == 'league':
-        
+
         for t in sa.columns:
             sa_1 = sa.loc['SA.1',f'{t}']
             esva_1 = esva.loc['ESVA.1',f'{t}']
             rating = -1.7051343491288098e-15 -0.19222278*sa_1 + 0.28562659*esva_1
             rating_lst.append(rating)
-        
+
         keys = sa.columns
         values = rating_lst
         r_dict = dict(zip(keys,values))
         rating_df = pd.DataFrame(r_dict,index=[0])
-        rating_df = pd.melt(rating_df,var_name='Team',value_name='Fw_rating')
+        rating_df = pd.melt(rating_df,var_name='Tm',value_name='Fw_rating')
+        rating_df['Tm'].replace({'L.A':'LAK','N.J':'NJD','S.J':'SJS','T.B':'TBL','VGK':'VEG'},inplace=True)
+        
         return rating_df
-    
-    
-        
-        
+
+    else:
+        print('Not Happening')
+
+
 def sp_ratings():
-    
-    #Function returns power play opportunities and power play goals by team and their 
-    #opponents on a per game basis in a dataframe.
-    
+
     t_lst = []
 
     team_lst = ['TBL','TOR','BOS','BUF','MTL','FLA','DET','OTT','WSH','PIT',
@@ -75,7 +71,7 @@ def sp_ratings():
         df['OT/SO'] = df['OT/SO'].fillna('reg')
         df['Tm'] = f'{team}'
         df.set_index(['Date'],inplace=True)
-    
+
         t_lst.append(df)
 
     season = pd.concat(t_lst)
@@ -91,14 +87,28 @@ def sp_ratings():
     d.columns = ['Tm','Gp']
     sp_summ = df.merge(d,on='Tm')
     sp_summ.drop(['GP'],axis=1,inplace=True)
-    
+
     sp_summ.set_index('Tm',inplace=True)
     for stat in sp_summ.columns:
         sp_summ[f'{stat}'] = sp_summ[f'{stat}']/sp_summ['Gp']
     rating = sp_summ.round(3)
     osp_rating = rating[['PPG','PPO']]
     dsp_rating = rating[['PPG.1','PPO.1']]
-    final_ratings = pd.merge(osp_rating,dsp_rating,on='Tm')
-    final_ratings.reset_index(inplace=True)
-    return final_ratings
+    fr = pd.merge(osp_rating,dsp_rating,on='Tm')
+    fr['PPG/O'] = fr['PPG'] / fr['PPO']
+    fr['oPPG/O'] = fr['PPG.1'] / fr['PPO.1']
+    fr.reset_index(inplace=True)
+    return fr[['Tm','PPO','PPO.1','PPG/O','oPPG/O']]
+
+def ovr_rankings():
+
+    ovr = fw_ratings('league')
+    st = sp_ratings()
+    df = pd.merge(ovr,st,on='Tm')
+    df['PPG/g'] = df['PPO']*df['PPG/O']
+    df['oPPG/g'] = df['PPO.1']*df['oPPG/O']
+    df['SPdiff'] = df['PPG/g'] - df['oPPG/g']
+    df['ovr_rating'] = df['Fw_rating'] + df['SPdiff']
+    
+    return df[['Tm','ovr_rating']].sort_values(by='ovr_rating',ascending=False).reset_index(drop=True)
 
